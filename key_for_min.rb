6ebc6b7e7fe670843972271f8e20ae1e806d7e0a
Maasa Kono <konomaasa@gmail.com>
2019-05-14 12:36:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,10 @@ def key_for_min_value(name_hash)
   smallest_value = nil
   smallest_key = nil
   name_hash.each do |key, value|
-    if previous_value == nil || value < smallest_value
+    if smallest_value == nil || value < smallest_value
       smallest_value = value 
       smallest_key = key 
     end 
   end 
-  smallest_key
+  smallest_key(name_hash)
 end
